57d0d75d7dc44f04eea20ae71a11316f0e7076ef
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2024-01-01 19:20:04 +0100

    Add new components and update CSS

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom"
+import { Header } from "../components"
 const HomeLayout = () => {
   return (
     <>
-    <nav>
-        <span className="text-4xl text-primary"></span>
-        </nav>
+     <Header />
+      <section className="align-element py-20">
         <Outlet />
-        </>
-  )
+      </section>
+    </>
+  );
 }
 
 export default HomeLayout
