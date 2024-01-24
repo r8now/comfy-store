ba215e5058526ebf45024b2c14cd4197cd746891
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2024-01-24 11:52:17 +0100

    Refactor About page layout and add additional content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,21 @@
 const About = () => {
-  return <h1 className="text-4xl">About</h1>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          About
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              conmfy about
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut commodi mollitia ipsa natus possimus at repudiandae quo animi exercitationem quod.</p>
+    </>
+  );
 };
 
 export default About;
